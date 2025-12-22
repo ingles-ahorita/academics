6492778da32c662311270b27092902c7e85ce3ca
Ruben Olivares <info@inglesahorita.com>
2025-12-22 16:55:16 +0100

    Revert to original landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import HelloWorld from './pages/HelloWorld';
 import './App.css';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/hello" element={<HelloWorld />} />
     </Routes>
   );
 }
