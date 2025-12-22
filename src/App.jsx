f2ad94097692c228185321c8cf6b28864d91c99c
Ruben Olivares <info@inglesahorita.com>
2025-12-22 16:55:26 +0100

    Update git author configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import HelloWorld from './pages/HelloWorld';
 import './App.css';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/hello" element={<HelloWorld />} />
     </Routes>
   );
 }
