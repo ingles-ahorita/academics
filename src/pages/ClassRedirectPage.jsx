fa9836f5f815a26f8c28ee5c82815c59a862ca88
Ruben Olivares <info@inglesahorita.com>
2025-12-30 20:53:01 +0100

    Restore ClassRedirectPage.jsx - fix Vercel build failure

--- src/pages/ClassRedirectPage.jsx
+++ src/pages/ClassRedirectPage.jsx
@@ -1,1 +1,6 @@
+import StudentAccessPage from './StudentAccessPage';
 
+export default function ClassRedirectPage() {
+  // Always show StudentAccessPage for class access, regardless of login status
+  return <StudentAccessPage />;
+}
